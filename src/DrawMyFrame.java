c80f0ff0b48da9c729fc8ed8888aaf74761abbe3
김동현 <oksean030330@gmail.com>
2024-05-31 01:06:12 +0900

    undo shortcut 추가 (command + z)

--- src/DrawMyFrame.java
+++ src/DrawMyFrame.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 public class DrawMyFrame extends JFrame
@@ -49,6 +50,9 @@ public class DrawMyFrame extends JFrame
     MenuHandler handler = new MenuHandler();
     for (int i = 0; i < arr.length; i++) {
       JMenuItem menuItem = new JMenuItem(arr[i]);
+      if (arr[i].contains("Undo")) {
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.META_MASK));
+      }
       menu.add(menuItem);
       menuItem.addActionListener(handler);
     }
@@ -97,7 +101,7 @@ public class DrawMyFrame extends JFrame
     public void actionPerformed(ActionEvent e) {
       String actionCommand = e.getActionCommand();
       switch (actionCommand) {
-        case "Undo":
+        case "Undo (Ctrl+Z)":
           panel.clearLastShape();
           break;
         case "Redo":
